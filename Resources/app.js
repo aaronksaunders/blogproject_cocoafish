1a0f847e7b0c3becbc6ca126d0d50f320233bf4e
aaronksaunders <aaron@clearlyinnovative.com>
2011-12-19 09:07:26 -0500

    Update Resources/app.js

--- Resources/app.js
+++ Resources/app.js
@@ -1,3 +1,11 @@
+/*
+  This is a sample project using Cocoafish and Titanium.
+  See additional code samples and documentation on my blog 
+  
+  Aaron K. Saunders
+  
+  @see http://blog.clearlyinnovative.com
+*/
 (function() {
 
 	//include Cocoafish & credentials module
