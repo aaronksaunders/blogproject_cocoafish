2a07a0e5342b2b13879cdaadc58d34114c06ba07
aaronksaunders <aaron@clearlyinnovative.com>
2011-12-19 09:08:05 -0500

    Update Resources/credentials.js

--- Resources/credentials.js
+++ Resources/credentials.js
@@ -1,3 +1,11 @@
+/*
+  This is a sample project using Cocoafish and Titanium.
+  See additional code samples and documentation on my blog 
+  
+  Aaron K. Saunders
+  
+  @see http://blog.clearlyinnovative.com
+*/
 exports.C = {
 	COCOAFISH_APP_NAME : 'YOURS',
 	COCOAFISH_API_KEY : "YOURS"
