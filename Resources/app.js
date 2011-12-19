12fc5348bd42c5abe34b6f6e038040557dc585dc
Aaron Saunders <aaron@clearlyinnovative.com>
2011-12-19 02:20:31 -0500

    login user on resume

--- Resources/app.js
+++ Resources/app.js
@@ -1,19 +1,41 @@
-//include Cocoafish & credentials module
-var credentials = require('credentials').C;
-var cocoafish = require('cocoafish_module');
+(function() {
 
-//create Cocoafish Client
-var client = new cocoafish.Client(credentials.COCOAFISH_APP_NAME, credentials.COCOAFISH_API_KEY);
+	//include Cocoafish & credentials module
+	var credentials = require('credentials').C;
+	var cocoafish = require('cocoafish_module');
 
-// login to the app
-client.login({
-	'login' : "aaron@clearlyinnovative.com",
-	'password' : "password"
-}, function(e) {
-	if(e.success === true) {
-		Ti.API.info("login " + JSON.stringify(e));
-		alert(JSON.stringify(e));
-	} else {
-		Ti.API.error(e.error);
-	}
-});
+	//create Cocoafish Client
+	var client = new cocoafish.Client(credentials.COCOAFISH_APP_NAME, credentials.COCOAFISH_API_KEY);
+
+	// login to the app
+	var params = {
+		'login' : "aaron@clearlyinnovative.com",
+		'password' : "password"
+	};
+	client.login(params, function(e) {
+		if(e.success === true) {
+			Ti.API.info("login " + JSON.stringify(e));
+			alert(JSON.stringify(e));
+
+			// save the credentials so they are available later
+			Ti.App.Properties.setString('CREDENTIALS', JSON.stringify(params));
+		} else {
+			Ti.API.error(e.error);
+		}
+	});
+	/**
+	 * event listener - on the resume event, try and log the user in
+	 */
+	Ti.App.addEventListener('resumed', function() {
+		var creds = JSON.parse(Ti.App.Properties.getString('CREDENTIALS'));
+		if(creds.login && creds.password) {
+			globals.client.doLogin(creds, function(e) {
+				if(e.success === false) {
+					makeSimpleAlert("Error Connecting to the Server, Please login again").show();
+				} else {
+					Ti.API.debug("Logged in on resume " + JSON.stringify(e.response));
+				}
+			});
+		}
+	});
+})();
